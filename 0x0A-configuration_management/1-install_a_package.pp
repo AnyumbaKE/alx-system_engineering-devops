3651fef3d180789e84ba0264f9f5690adf706d86
AnyumbaKE <esanyumba@gmail.com>
2023-12-22 14:49:21 -0800

    upt

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,25 @@
-# manifest.pp
+# install flask from pip3
 
-package { 'python3-pip':
-  ensure => installed,
+# ensure that python present 
+package { 'python3.8':
+  ensure => present,
 }
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  unless  => '/usr/bin/pip3 show flask | grep Version | grep -q "2.1.0"',
-  require => Package['python3-pip'],
+# ensure that pip present
+package { 'python3-pip':
+  ensure => present,
+}
+
+# Install Flask 
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+# Install Werkzeug 
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }

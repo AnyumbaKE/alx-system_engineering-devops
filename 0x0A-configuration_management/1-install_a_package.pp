95f5e8c7a375873799b1f9754d7a0bbc28ab4cfe
AnyumbaKE <esanyumba@gmail.com>
2023-12-22 20:37:54 +0300

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,6 @@
 # Install flask using puppet
 
-# install_flask.pp
-
-# Ensure that the 'python3-pip' package is installed
-package { 'python3-pip':
-  ensure => 'installed',
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
-
-# Use pip3 to install Flask version 2.1.0
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin'],
-  require => Package['python3-pip'],
-}
-
-

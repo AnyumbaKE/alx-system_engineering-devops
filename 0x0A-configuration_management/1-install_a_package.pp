8ae36bc46c2a4e7fe0b6602d57019be890e639ee
AnyumbaKE <esanyumba@gmail.com>
2023-12-22 09:33:36 -0800

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,17 @@
 # Install flask using puppet
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# install_flask.pp
+
+# Ensure that the 'python3-pip' package is installed
+package { 'python3-pip':
+  ensure => 'installed',
 }
+
+# Use pip3 to install Flask version 2.1.0
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  require => Package['python3-pip'],
+}
+
 

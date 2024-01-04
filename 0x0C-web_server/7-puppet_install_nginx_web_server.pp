4abde26a7d63b0e243ef633f0e7dd2596cc631ee
AnyumbaKE <esanyumba@gmail.com>
2024-01-03 16:10:17 -0800

    5. Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,10 +3,10 @@
 
 #   Nginx should be listening on port 80
 #   When querying Nginx at its root / with a GET request (requesting a page) 
-        using curl, it must return a page that contains the string Hello World!
+#        using curl, it must return a page that contains the string Hello World!
 #   The redirection must be a “301 Moved Permanently”
 #   Your answer file should be a Puppet manifest containing commands to automatically
-        configure an Ubuntu machine to respect above requirements
+#        configure an Ubuntu machine to respect above requirements
 
 exec {'install':
   provider => shell,
